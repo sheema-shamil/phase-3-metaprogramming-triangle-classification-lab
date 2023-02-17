2527ced8a9ffc44c0d44629bed09ebc802e5efea
sheema <sheemashamilfarook@gmail.com>
2023-02-17 17:19:41 +0300

    done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
   # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    triangle_validation
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def positive_number?
+    [a, b, c].all?(&:positive?)
+  end
+
+  def triangle_inequality?
+    a + b > c && a + c > b && b + c > a
+  end
+
+  def triangle_validation
+    raise TriangleError unless positive_number? && triangle_inequality?
+  end
+
+  class TriangleError < StandardError
+  end
 end
